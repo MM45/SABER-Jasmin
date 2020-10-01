7b5ebe29f7d947a795ac7601a5e5b7e1d20b0c9c
MM45 <m.c.f.h.p.meijers@student.tue.nl>
2020-10-01 15:16:31 +0200

    Start on AVX2 version of KeccakF1600_StatePermute.

--- avx/GENERAL/auxilliary.h
+++ avx/GENERAL/auxilliary.h
@@ -4,3 +4,5 @@
 
 __m256i load_16u16_jazz(uint16_t buf[16]);
 void store_16u16_jazz(__m256i* r, uint16_t* buf);
+__m256i andnot_256_jazz(__m256i* a, __m256i* b);
+__m256i load_4u64_jazz(uint64_t buf[4]);
